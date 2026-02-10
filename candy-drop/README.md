103043ed7db2b5d502db671e34f5485115bb574c
Lazybones3 <caorui452@163.com>
2026-02-10 16:43:36 +0800

    candy-drop

--- candy-drop/README.md
+++ candy-drop/README.md
@@ -1,5 +1,13 @@
 # Develop a full web app to sell your custom NFTs
 
-What's a non-fungible token?
+## What's a non-fungible token?
 
 Example: Each NFT is signed with Picasso's Solana wallet. Each NFT is unique.
+
+## Running following commands to start this project:
+
+```
+nvm use 16
+npm install
+npm run start
+```
--- candy-drop/src/App.js
+++ candy-drop/src/App.js
@@ -1,19 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
+import CandyMachine from "./CandyMachine";
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+	const [walletAddress, setWalletAddress] = useState(null);
+	const checkIfWalletIsConnected = async () => {
+		try {
+			const { solana } = window;
+			if (solana) {
+				if (solana.isPhantom) {
+					console.log("Phantom wallet found!");
+					const response = await solana.connect({
+						onlyIfTrusted: true,
+					});
+					console.log("Connected with Public Key: ", response.publicKey.toString());
+					setWalletAddress(response.publicKey.toString());
+				}
+			} else {
+				alert("Solana object not found! Get a Phantom Wallet!");
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}
+	const connectWallet = async () => {
+		const { solana } = window;
+		if (solana) {
+			const response = await solana.connect();
+			console.log('Connected with Public Key:', response.publicKey.toString());
+			setWalletAddress(response.publicKey.toString());
+		}
+	};
+
+	const renderNotConnetedContainer = () => {
+		return <button className="cta-button connect-wallet-button" onClick={connectWallet}>Connect to wallet</button>
+	};
+	useEffect(() => {
+		const onLoad = async () => {
+			await checkIfWalletIsConnected();
+		}
+		window.addEventListener("load", onLoad);
+		return () => window.removeEventListener("load", onLoad);
+	}, []);
 	return (
 		<div className="App">
 			<div className="container">
 				<div className="header-container">
 					<p className="header">🍭 Candy Drop</p>
 					<p className="sub-text">NFT drop machine with fair mint</p>
+					{!walletAddress && renderNotConnetedContainer()}
 				</div>
+				{walletAddress && <CandyMachine walletAddress={window.solana}/>}
 				<div className="footer-container">
 					<img
 						alt="Twitter Logo"
